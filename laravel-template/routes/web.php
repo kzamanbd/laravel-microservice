41c3a3d9d627a74e423b96baae7868b66c4c534b
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-06-30 00:20:59 +0600

    fix: template route

--- laravel-template/routes/web.php
+++ laravel-template/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Laravel Version' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
